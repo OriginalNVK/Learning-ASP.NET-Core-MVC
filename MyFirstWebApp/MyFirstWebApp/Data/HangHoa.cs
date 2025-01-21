bdff45d4a9dc296f6f43c3c15dfcb8395da2cc8a
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2025-01-21 10:21:39 +0700

    Add connection string and install some package

---
+++ MyFirstWebApp/MyFirstWebApp/Data/HangHoa.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyFirstWebApp.Data
+{
+    [Table("HangHoa")]
+    public class HangHoa
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string TenHH {  get; set; }
+
+        [Required]
+        [Range(0, Double.MaxValue)]
+        public double GiaHH { get; set; }
+
+        public string? Mota { get; set; }
+
+        public int? MaLoai { get; set; }
+        [ForeignKey("MaLoai")]
+        public Loai Loai { get; set; }
+    }
+}
