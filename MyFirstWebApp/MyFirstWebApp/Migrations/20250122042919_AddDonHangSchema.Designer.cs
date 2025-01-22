e02b8dd37e68d13feeae0ae9cd442a47490b24e0
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2025-01-22 11:33:27 +0700

    Add Entity DonHang and DonHangChiTiet then use fluent API to create connect all entities

--- MyFirstWebApp/MyFirstWebApp/Migrations/20250122042919_AddDonHangSchema.Designer.cs
+++ MyFirstWebApp/MyFirstWebApp/Migrations/20250122042919_AddDonHangSchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyFirstWebApp.Data;
 
@@ -11,9 +12,11 @@ using MyFirstWebApp.Data;
 namespace MyFirstWebApp.Migrations
 {
     [DbContext(typeof(WebDbContext))]
-    partial class WebDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250122042919_AddDonHangSchema")]
+    partial class AddDonHangSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
