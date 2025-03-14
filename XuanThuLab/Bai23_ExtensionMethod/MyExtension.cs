66d0dafd89db85a4c1b745f838075505c0c7ed91
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2025-03-14 22:41:29 +0700

    Learning Extension Method

---
+++ XuanThuLab/Bai23_ExtensionMethod/MyExtension.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MyLib
+{
+    public static class Xyz
+    {
+        public static double BinhPhuong(this double x)
+        {
+            return x * x;
+        }
+
+        public static double CanBac2(this double x) => Math.Sqrt(x);
+
+        public static double Sin(this double x) => Math.Sin(x);
+        public static double Cos (this double x) => Math.Cos(x);
+        
+
+    }
+}
---
+++ XuanThuLab/Bai23_ExtensionMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MyLib;
+
+namespace Bai23
+{
+    // extension method:
+    static class Abc
+    {
+        public static void Print(this string s, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+    } 
+    class Program
+    {
+
+        static void Main()
+        {
+            // int[] mang = { 1, 2, 3, 4 };
+            // int max = mang.Max();
+            // Console.WriteLine("Max: " + max);
+            // string s = "Xin chao cac ban";
+            // s.Print(ConsoleColor.Blue);
+
+            double a = 2.5;
+            Console.WriteLine(a.BinhPhuong());
+            Console.WriteLine(a.CanBac2());
+            Console.WriteLine(a.Sin());
+            Console.WriteLine(a.Cos());
+
+        }
+    }
+}
